c731612f24f566563a8b84dd323f0b75fbfa5c9f
Robert "Ross" Denman II - RoosDaMan <denman.ross@gmail.com>
2021-07-08 17:39:39 -0400

    Add project files.

---
+++ TheInvestorCompound.Data/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInvestorCompound.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        [Display(Name = "Posted By")]
+        public string PostedBy { get; set; }
+        [Required]
+        [MaxLength(160, ErrorMessage = "Please use 160 characters or less")]
+        [Display(Name = "Title")]
+        public string PostName { get; set; }
+        public string PostCoverImage { get; set; }
+        [Required]
+        public string PostContent { get; set; }
+        [Required]
+        [Display(Name = "Created (UTC)")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name = "Last Modified (UTC)")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+    }
+}
